2696e0bebde1b062cb49cccb76ab059d64a66914
Yoyo <maonome@gmail.com>
2022-04-25 12:23:06 +0800

    done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,84 @@
 import './style.scss';
 
-console.log(window);
+
+window.onload = ()=>{
+    const mode = document.getElementById("mode");
+    const result = document.getElementById("result");
+
+    
+
+    document.getElementById ("add").addEventListener ("click", ()=>{
+        mode.innerHTML = "+";
+
+    const first = (document.getElementById("first") as HTMLInputElement).value;
+    const latter = (document.getElementById("latter") as HTMLInputElement).value;
+    const result = document.getElementById("result");
+    result.innerHTML = String((+first + +latter).toFixed(2));
+    if(+first === 0 && +latter === 0){
+        result.innerHTML = String(0);
+    }
+    }, false);
+
+    document.getElementById ("reduce").addEventListener ("click", ()=>{
+        mode.innerHTML = "-";
+        const first = (document.getElementById("first") as HTMLInputElement).value;
+        const latter = (document.getElementById("latter") as HTMLInputElement).value;
+        const result = document.getElementById("result");
+        result.innerHTML = String((+first - +latter).toFixed(2));
+        if(+first === 0 && +latter === 0){
+            result.innerHTML = String(0);
+        }
+        if(result.innerHTML === "Infinity"){
+            result.innerHTML = "錯誤";
+        }
+    
+    }, false);
+
+    document.getElementById ("multiplication").addEventListener ("click", ()=>{
+        mode.innerHTML = "x";
+
+    const first = (document.getElementById("first") as HTMLInputElement).value;
+    const latter = (document.getElementById("latter") as HTMLInputElement).value;
+    const result = document.getElementById("result");
+    result.innerHTML = String((+first * +latter).toFixed(2));
+    if(+first === 0 && +latter === 0){
+        result.innerHTML = String(0);
+    }
+    if(result.innerHTML === "Infinity"){
+        result.innerHTML = "錯誤";
+    }
+
+    }, false);
+
+    document.getElementById ("divide").addEventListener ("click", ()=>{
+        mode.innerHTML = "/";
+
+    const first = (document.getElementById("first") as HTMLInputElement).value;
+    const latter = (document.getElementById("latter") as HTMLInputElement).value;
+    const result = document.getElementById("result");
+    result.innerHTML = String((+first / +latter).toFixed(2));
+    if(+first === 0 && +latter === 0){
+        result.innerHTML = String(0);
+    }
+    if(result.innerHTML === "Infinity"){
+        result.innerHTML = "錯誤";
+    }
+
+    }, false);
+
+    document.getElementById ("zero").addEventListener ("click", ()=>{
+        mode.innerHTML = "+";
+        let first = (document.getElementById("first") as HTMLInputElement);
+        let latter = (document.getElementById("latter") as HTMLInputElement);
+        const result = document.getElementById("result");
+        
+        first.value = String(0);
+        latter.value = String(0);
+        result.innerHTML = String(0);    
+        if(result.innerHTML === "Infinity"){
+            result.innerHTML = "錯誤";
+        }
+    
+    }, false);
+
+}

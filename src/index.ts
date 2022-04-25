c3c05ddacb78ffe367c051e231ef6e8d605e4b4b
yoyo <maonome@gmail.com>
2022-04-25 16:42:08 +0800

    dnoe

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,63 @@
 import './style.scss';
 
+window.onload = () => {
+  const mode = document.getElementById('mode');
+  const result = document.getElementById('result');
 
-window.onload = ()=>{
-    const mode = document.getElementById("mode");
-    const result = document.getElementById("result");
+  document.getElementById('add').addEventListener('click', () => {
+    mode.innerHTML = '+';
 
-    
+    const first = (document.getElementById('first') as HTMLInputElement).value;
+    const latter = (document.getElementById('latter') as HTMLInputElement).value;
 
-    document.getElementById ("add").addEventListener ("click", ()=>{
-        mode.innerHTML = "+";
-
-    const first = (document.getElementById("first") as HTMLInputElement).value;
-    const latter = (document.getElementById("latter") as HTMLInputElement).value;
-    const result = document.getElementById("result");
     result.innerHTML = String((+first + +latter).toFixed(2));
-    if(+first === 0 && +latter === 0){
-        result.innerHTML = String(0);
+    if (+first === 0 && +latter === 0) {
+      result.innerHTML = String(0);
     }
-    }, false);
+  }, false);
 
-    document.getElementById ("reduce").addEventListener ("click", ()=>{
-        mode.innerHTML = "-";
-        const first = (document.getElementById("first") as HTMLInputElement).value;
-        const latter = (document.getElementById("latter") as HTMLInputElement).value;
-        const result = document.getElementById("result");
-        result.innerHTML = String((+first - +latter).toFixed(2));
-        if(+first === 0 && +latter === 0){
-            result.innerHTML = String(0);
-        }
-        if(result.innerHTML === "Infinity"){
-            result.innerHTML = "錯誤";
-        }
-    
-    }, false);
+  document.getElementById('reduce').addEventListener('click', () => {
+    mode.innerHTML = '-';
+    const first = (document.getElementById('first') as HTMLInputElement).value;
+    const latter = (document.getElementById('latter') as HTMLInputElement).value;
+    result.innerHTML = String((+first - +latter).toFixed(2));
+    if (result.innerHTML === 'Infinity') {
+      result.innerHTML = '錯誤';
+    }
+  }, false);
 
-    document.getElementById ("multiplication").addEventListener ("click", ()=>{
-        mode.innerHTML = "x";
+  document.getElementById('multiplication').addEventListener('click', () => {
+    mode.innerHTML = 'x';
 
-    const first = (document.getElementById("first") as HTMLInputElement).value;
-    const latter = (document.getElementById("latter") as HTMLInputElement).value;
-    const result = document.getElementById("result");
+    const first = (document.getElementById('first') as HTMLInputElement).value;
+    const latter = (document.getElementById('latter') as HTMLInputElement).value;
     result.innerHTML = String((+first * +latter).toFixed(2));
-    if(+first === 0 && +latter === 0){
-        result.innerHTML = String(0);
+    if (result.innerHTML === 'Infinity') {
+      result.innerHTML = '錯誤';
     }
-    if(result.innerHTML === "Infinity"){
-        result.innerHTML = "錯誤";
-    }
+  }, false);
 
-    }, false);
+  document.getElementById('divide').addEventListener('click', () => {
+    mode.innerHTML = '/';
 
-    document.getElementById ("divide").addEventListener ("click", ()=>{
-        mode.innerHTML = "/";
-
-    const first = (document.getElementById("first") as HTMLInputElement).value;
-    const latter = (document.getElementById("latter") as HTMLInputElement).value;
-    const result = document.getElementById("result");
+    const first = (document.getElementById('first') as HTMLInputElement).value;
+    const latter = (document.getElementById('latter') as HTMLInputElement).value;
     result.innerHTML = String((+first / +latter).toFixed(2));
-    if(+first === 0 && +latter === 0){
-        result.innerHTML = String(0);
+    if (result.innerHTML === 'Infinity') {
+      result.innerHTML = '錯誤';
     }
-    if(result.innerHTML === "Infinity"){
-        result.innerHTML = "錯誤";
+  }, false);
+
+  document.getElementById('zero').addEventListener('click', () => {
+    mode.innerHTML = '+';
+    const first = (document.getElementById('first') as HTMLInputElement);
+    const latter = (document.getElementById('latter') as HTMLInputElement);
+
+    first.value = String(0);
+    latter.value = String(0);
+    result.innerHTML = String(0);
+    if (result.innerHTML === 'Infinity') {
+      result.innerHTML = '錯誤';
     }
-
-    }, false);
-
-    document.getElementById ("zero").addEventListener ("click", ()=>{
-        mode.innerHTML = "+";
-        let first = (document.getElementById("first") as HTMLInputElement);
-        let latter = (document.getElementById("latter") as HTMLInputElement);
-        const result = document.getElementById("result");
-        
-        first.value = String(0);
-        latter.value = String(0);
-        result.innerHTML = String(0);    
-        if(result.innerHTML === "Infinity"){
-            result.innerHTML = "錯誤";
-        }
-    
-    }, false);
-
-}
+  }, false);
+};
